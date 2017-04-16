d7a5e9216d6523d85361989240c2cb7944f09b52
danaebentley <danabentley@outlook.com>
2017-04-16 12:30:15 -0400

    Adding some scripts from lessons online

---
+++ interactive.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+echo "Hey what's your first name?";
+read a;
+echo "Welcome $a, would you like to tell us your last name?";
+read b;
+echo "Thanks $a $b for telling us your name";
+echo "*************************************"
+echo "$a $b it is time to say goodbye"
---
+++ process.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Hello $USER"
+echo "Hey I am" $USER "and will be telling you about the current processes"
+echo "Running processes list"
+ps
